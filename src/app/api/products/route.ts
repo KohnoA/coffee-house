92ff5e198d9d10c21529c7d2b673b63d68ef76e9
KohnoA <san.kohno@yandex.ru>
2023-12-24 21:30:25 +0300

    feat: update api

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,6 +1,31 @@
-import { NextResponse } from 'next/server';
-import products from '@/assets/data/products.json';
+import { NextRequest, NextResponse } from 'next/server';
+import productsData from '@/db/products.json';
 
-export async function GET(request: Request) {
-  return NextResponse.json(products);
+export async function GET(request: NextRequest) {
+  const params = request.nextUrl.searchParams;
+
+  if (params.has('category')) {
+    const category = params.get('category');
+    const response = productsData.filter(
+      (product) => product.category === category
+    );
+
+    return !!response.length
+      ? NextResponse.json(response)
+      : NextResponse.json({ error: 'This category not found' }, { status: 404 });
+  }
+
+  if (params.has('id')) {
+    const id = params.get('id');
+    const response = productsData.find((product) => String(product.id) === id);
+
+    return !!response
+      ? NextResponse.json(response)
+      : NextResponse.json(
+          { error: 'A product with this ID does not exist' },
+          { status: 404 }
+        );
+  }
+
+  return NextResponse.json(productsData);
 }
