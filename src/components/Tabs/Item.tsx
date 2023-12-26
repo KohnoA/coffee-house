c858f1929d443a8b647bd3010de7c8fcfbc62c81
KohnoA <san.kohno@yandex.ru>
2023-12-26 11:42:16 +0300

    refactor: Tabs component

--- src/components/Tabs/Item.tsx
+++ src/components/Tabs/Item.tsx
@@ -1,75 +1,35 @@
 import Button from '@/UI/Button';
-import { ReactNode, useContext, useState } from 'react';
-import { TabsContext } from './context';
-import { useEffectWithoutMount } from '@/hooks';
-import { DEFAULT_VALUE_FOR_TABS } from './constants';
+import { ReactNode } from 'react';
 
 interface ItemProps {
   icon?: ReactNode;
-  children: string;
+  label: string;
   value: string;
+  isActive: boolean;
+  onClick: (value: string) => void;
 }
 
-export default function Item({ value, icon, children }: ItemProps) {
-  const { activeTabs, multiply, setActiveTabs } = useContext(TabsContext);
-  const [isActive, setIsActive] = useState<boolean>(activeTabs.includes(value));
-
-  const setActiveTabsHandler = () => {
-    if (multiply) {
-      if (Array.isArray(activeTabs)) {
-        setActiveTabs([...activeTabs, value]);
-      } else {
-        setActiveTabs([activeTabs, value]);
-      }
-    } else {
-      setActiveTabs(value);
-    }
-  };
-
-  const filterActiveTabsHanlder = () => {
-    if (Array.isArray(activeTabs)) {
-      const filteredActiveTabs = activeTabs.filter((tab) => tab !== value);
-
-      setActiveTabs(
-        filteredActiveTabs.length ? filteredActiveTabs : DEFAULT_VALUE_FOR_TABS
-      );
-    } else {
-      setActiveTabs(DEFAULT_VALUE_FOR_TABS);
-    }
-  };
-
-  const onClickHandler = () => {
-    if (!multiply && isActive) return;
-
-    if (isActive) {
-      filterActiveTabsHanlder();
-    } else {
-      setActiveTabsHandler();
-    }
-  };
-
-  useEffectWithoutMount(() => {
-    setIsActive(activeTabs.includes(value));
-  }, [activeTabs, value]);
+export default function Item(props: ItemProps) {
+  const { value, label, icon, isActive, onClick } = props;
 
   return (
     <li>
       <Button
-        onClick={onClickHandler}
+        onClick={() => onClick(value)}
         className={`group flex items-center gap-[8px] py-[8px] pl-[8px] pr-[16px] border-borderLight hover:border-bgContainer ${
           isActive ? 'text-textLight !bg-bgContainer !border-bgContainer' : ''
         }`}
       >
         {icon && (
           <span
-            className={`flex justify-center items-center w-[30px] h-[30px] p-[5px] rounded-full transition-colors bg-borderLight group-hover:bg-bgBody ${
+            className={`flex justify-center items-center w-[30px] h-[30px] p-[5px] rounded-full text-textDark transition-colors bg-borderLight group-hover:bg-bgBody ${
               isActive ? '!bg-bgBody' : ''
             }`}
           >
             {icon}
           </span>
         )}{' '}
-        {children}
+        {label}
       </Button>
     </li>
   );
--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,40 +1,79 @@
 'use client';
 
-import { ReactNode, useEffect, useState } from 'react';
-import { TabsContext } from './context';
+import { ReactNode, memo, useState } from 'react';
 import Item from './Item';
-import { DEFAULT_VALUE_FOR_TABS } from './constants';
+import { useEffectWithoutMount } from '@/hooks';
 
 interface TabsProps {
-  children: ReactNode;
   multiply?: boolean;
   defaultValue?: string | string[];
   className?: string;
+  items: { label: string; value: string; icon?: string | ReactNode }[];
   onChange: (value: string | string[]) => void;
 }
 
-export default function Tabs(props: TabsProps) {
-  const {
-    children,
-    multiply = false,
-    defaultValue,
-    className,
-    onChange,
-  } = props;
+const DEFAULT_VALUE_FOR_TABS = '';
+
+function Tabs(props: TabsProps) {
+  const { multiply = false, defaultValue, items, className, onChange } = props;
 
   const [activeTabs, setActiveTabs] = useState<string | string[]>(
     defaultValue ?? DEFAULT_VALUE_FOR_TABS
   );
 
-  useEffect(() => {
+  const setActiveTabsHandler = (value: string) => {
+    if (multiply) {
+      if (Array.isArray(activeTabs)) {
+        setActiveTabs([...activeTabs, value]);
+      } else {
+        setActiveTabs([activeTabs, value]);
+      }
+    } else {
+      setActiveTabs(value);
+    }
+  };
+
+  const filterActiveTabsHanlder = (value: string) => {
+    if (Array.isArray(activeTabs)) {
+      const filteredActiveTabs = activeTabs.filter((tab) => tab !== value);
+
+      setActiveTabs(
+        filteredActiveTabs.length ? filteredActiveTabs : DEFAULT_VALUE_FOR_TABS
+      );
+    } else {
+      setActiveTabs(DEFAULT_VALUE_FOR_TABS);
+    }
+  };
+
+  const onClickHandler = (value: string) => {
+    const isActiveValue = activeTabs.includes(value);
+
+    if (!multiply && isActiveValue) return;
+
+    if (isActiveValue) {
+      filterActiveTabsHanlder(value);
+    } else {
+      setActiveTabsHandler(value);
+    }
+  };
+
+  useEffectWithoutMount(() => {
     onChange(activeTabs);
   }, [activeTabs, onChange]);
 
   return (
-    <TabsContext.Provider value={{ activeTabs, multiply, setActiveTabs }}>
-      <ul className={`flex gap-[16px] ${className ?? ''}`}>{children}</ul>
-    </TabsContext.Provider>
+    <ul className={`flex gap-[16px] ${className ?? ''}`}>
+      {items.map(({ value, ...otherProps }) => (
+        <Item
+          key={value}
+          value={value}
+          onClick={onClickHandler}
+          isActive={activeTabs.includes(value)}
+          {...otherProps}
+        />
+      ))}
+    </ul>
   );
 }
 
-Tabs.Item = Item;
+export default memo(Tabs);
